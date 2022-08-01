3127fe32e6a3a5d5d4637df8401092c49d762525
pedro henrique savarezzi maria <pedro.avare01@gmail.com>
2022-08-01 20:32:26 -0300

    Changes input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,16 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  icon: React.ElementType;
+}
+
+function Input({ name, icon: Icon, ...rest }: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -48,6 +48,6 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
+}
 
 export default Input;
